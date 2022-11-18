9210a90484f061bacbe9c27e5e235e5b074578ae
cristian m <camunoz2@gmail.com>
2022-11-18 12:31:44 -0300

    fixed blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -11,5 +11,11 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()]
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "github-dark-dimmed",
+    },
+  },
+  integrations: [tailwind(), react(), mdx()],
 });
