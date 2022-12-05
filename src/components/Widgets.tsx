9bb21022547f9cdac05e0589789116c1f2709af9
cristian <camunoz2@gmail.com>
2022-12-05 14:32:00 -0300

    add dribbble folio link

--- src/components/Widgets.tsx
+++ src/components/Widgets.tsx
@@ -8,10 +8,15 @@ const Widgets = () => {
     <div className="grid grid-cols-3 grid-rows-3 lg:grid-rows-2 gap-4 h-full">
       <FrontEndFolio />
       <div className="py-4 px-4 col-start-2 col-span-1 row-start-1 row-span-3 lg:col-start-3 lg:row-start-1 lg:col-span-1 lg:row-span-2 flex rounded-xl items-center overflow-hidden bg-slate-200 justify-around lg:flex-col flex-row">
-        <div className="flex flex-col items-center gap-2">
+        <a
+          href="https://dribbble.com/arjeldev"
+          target="_blank"
+          rel="noopener"
+          className="flex flex-col items-center gap-2"
+        >
           <img src={dribbble} alt="" />
           <h2 className="hidden lg:block font-bold">dribbble</h2>
-        </div>
+        </a>
         <div className="flex flex-col items-center gap-2">
           <img src={codepen} alt="" />
           <h2 className="font-bold hidden lg:block">codepen</h2>
