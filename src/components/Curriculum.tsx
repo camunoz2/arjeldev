a6f31723782a6acb2c379c2c2c1ebdede0c63aaa
cristian m <camunoz2@gmail.com>
2023-06-20 21:50:11 -0400

    uploaded my cv

---
+++ src/components/Curriculum.tsx
@@ -0,0 +1,7 @@
+export function Curriculum() {
+  return (
+    <a href="https://arjeldev-projects.s3.sa-east-1.amazonaws.com/cv_cristian.a_fullstack.pdf" download={true} className="rounded-lg bg-white flex items-center justify-center font-bold cursor-pointer hover:bg-purple-600 hover:text-purple-400 hover:border hover:border-purple-800 transition-all hover:shadow-xl hover:shadow-purple-400 col-span-3 lg:col-span-1 py-6 lg:py-0">
+      CV
+    </a>
+  );
+}
--- src/components/Widgets.tsx
+++ src/components/Widgets.tsx
@@ -2,10 +2,11 @@ import FrontEndFolio from "./FrontEndFolio";
 import ProfileImage from "./ProfileImage";
 import dribbble from "../images/dribbble.webp";
 import codepen from "../images/codepen.webp";
+import { Curriculum } from "./Curriculum";
 
 const Widgets = () => {
   return (
-    <div className="grid grid-cols-3 grid-rows-3 lg:grid-rows-2 gap-4 h-full">
+    <div className="grid grid-cols-3 grid-rows-2 lg:grid-rows-2 gap-4 h-full">
       <FrontEndFolio />
       <div className="py-4 px-4 col-start-2 col-span-1 row-start-1 row-span-3 lg:col-start-3 lg:row-start-1 lg:col-span-1 lg:row-span-2 flex rounded-xl items-center overflow-hidden bg-slate-200 justify-around lg:flex-col flex-row">
         <a
@@ -23,8 +24,8 @@ const Widgets = () => {
           <h2 className="font-bold hidden lg:block">codepen</h2>
         </a>
       </div>
-
       <ProfileImage widget />
+      <Curriculum />
     </div>
   );
 };
